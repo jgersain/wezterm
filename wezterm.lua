34f709c05d2de73088703cc8be15896a8caf4663
jgersain <jhersein@gmail.com>
2026-05-09 09:57:20 -0600

    Update wexterm style

--- wezterm.lua
+++ wezterm.lua
@@ -1,18 +1,50 @@
--- Pull in the wezterm API
 local wezterm = require 'wezterm'
-
--- This will hold the configuration.
-local config = wezterm.config_builder()
 local act = wezterm.action
 
-local config = {}
-local backgroundConfig = require 'background_config'
+local config = wezterm.config_builder()
 
-backgroundConfig.set_background(config)
--- config.default_domain = 'WSL:Arch' -- wsl stuff
-config.font =
-  wezterm.font('JetBrains Mono', { weight = 'Bold' })
-config.font_size = 14.0
+-- Match VS Code's integrated terminal from the "Popping and Locking" theme.
+config.font = wezterm.font_with_fallback {
+  'Meslo LG S for Powerline',
+  'JetBrains Mono',
+}
+config.font_size = 18.0
+
+config.colors = {
+  foreground = '#ebdbb2',
+  background = '#181921',
+  cursor_bg = '#f42c3e',
+  cursor_fg = '#181921',
+  cursor_border = '#f42c3e',
+  selection_fg = '#ebdbb2',
+  selection_bg = '#334b5a',
+  scrollbar_thumb = '#928374',
+
+  ansi = {
+    '#1d2021',
+    '#cc241d',
+    '#98971a',
+    '#d79921',
+    '#458588',
+    '#b16286',
+    '#689d6a',
+    '#a89984',
+  },
+  brights = {
+    '#928374',
+    '#f42c3e',
+    '#b8bb26',
+    '#fabd2f',
+    '#99c6ca',
+    '#d3869b',
+    '#7ec16e',
+    '#ebdbb2',
+  },
+}
+
+config.cursor_blink_rate = 800
+config.enable_scroll_bar = true
+config.min_scroll_bar_height = '2cell'
 config.inactive_pane_hsb = {
   saturation = 0.9,
   brightness = 0.6,
@@ -25,37 +57,37 @@ config.keys = {
     mods = 'SHIFT|ALT',
     action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
   },
-	{
-    key = "DownArrow",
-    mods = "SHIFT|ALT",
-    action = wezterm.action.SplitVertical { domain = "CurrentPaneDomain" },
+  {
+    key = 'DownArrow',
+    mods = 'SHIFT|ALT',
+    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
   },
   {
     key = 'z',
     mods = 'CTRL',
     action = wezterm.action.TogglePaneZoomState,
   },
-	{
-		key = 'UpArrow', 
-		mods = 'CTRL', 
-		action = act.ScrollByPage(-0.1)
-	},
-	{
-		key = 'DownArrow', 
-		mods = 'CTRL', 
-		action = act.ScrollByPage(0.1)
-	},
-	{
+  {
+    key = 'UpArrow',
+    mods = 'CTRL',
+    action = act.ScrollByPage(-0.1),
+  },
+  {
+    key = 'DownArrow',
+    mods = 'CTRL',
+    action = act.ScrollByPage(0.1),
+  },
+  {
     key = 'UpArrow',
     mods = 'ALT',
     action = act.ActivatePaneDirection 'Up',
   },
-	{
+  {
     key = 'DownArrow',
     mods = 'ALT',
     action = act.ActivatePaneDirection 'Down',
   },
-	{
+  {
     key = 'LeftArrow',
     mods = 'ALT',
     action = act.ActivatePaneDirection 'Left',
@@ -67,8 +99,4 @@ config.keys = {
   },
 }
 
--- For example, changing the color scheme:
-config.color_scheme = 'AdventureTime'
-
--- and finally, return the configuration to wezterm
 return config
